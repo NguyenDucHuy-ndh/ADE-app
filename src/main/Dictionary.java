6dd4a3fc9188843c07506f456a9311e959635e14
NguyenDucHuy <nguyenduchuy2004ndh@gmail.com>
2023-10-11 16:55:14 +0700

    s

---
+++ src/main/Dictionary.java
@@ -0,0 +1,19 @@
+package main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+public class Dictionary {
+    private ObservableList<Word> wordList;
+
+    public Dictionary() {
+        wordList = FXCollections.observableArrayList();
+    }
+
+    public void addWord(Word word) {
+        wordList.add(word);
+    }
+
+    public ObservableList<Word> getWordList() {
+        return wordList;
+    }
+
+}
