8e5ab0bc6fe7b6b5abc12d46e1dab548ba9c564b
Nguyen_Tien_Dung <ngtiendung0105204@gmail.com>
2023-11-23 14:48:20 +0700

    add fxml

--- src/main/java/DisplayController.java
+++ src/main/java/DisplayController.java
@@ -123,4 +123,9 @@ public class DisplayController implements Initializable {
     }
 
 
+    public void exist(ActionEvent actionEvent) {
+    }
+
+    public void translate(ActionEvent actionEvent) {
+    }
 }
