e2fae98e4ab16955d7272e1f333e2b39261b085e
NguyenDucHuy <nguyenduchuy2004ndh@gmail.com>
2023-11-26 19:05:04 +0700

    fix edit

---
+++ src/JAVA/main/java/Main.java
@@ -0,0 +1,20 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Word word1 = new Word("Hello", "Xin chào");
+        Word word2 = new Word("House", "Ngôi nhà");
+        Word word3 = new Word("Love", "Yêu thương");
+
+        Dictionary dictionary = new Dictionary();
+
+        dictionary.addWord(word3);
+        dictionary.addWord(word1);
+        dictionary.addWord(word2);
+
+        DictionaryCommandline d = new DictionaryCommandline(dictionary);
+        d.dictionaryAdvanced();
+    }
+
+}
